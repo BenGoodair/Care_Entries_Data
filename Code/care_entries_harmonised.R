0dec35cc0334a1920be42b2b78fd89c84f05f6c4
Ben Goodair <ben.goodair@outlook.com>
2025-09-25 10:58:36 +0100

    little bit

--- Code/care_entries_harmonised.R
+++ Code/care_entries_harmonised.R
@@ -26,14 +26,50 @@ dplyr::mutate(LA_Name = LA_Name %>%
                 str_trim())
 
 
-df_raw <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/childrens_social_care_data/refs/heads/main/Final_Data/outputs/dashboard_data.csv"))
-
 df <- characteristics %>%
-  dplyr::filter(category=="started during")
+  dplyr::mutate(LA_Name = LA_Name %>%
+                  gsub('&', 'and', .) %>%
+                  gsub('[[:punct:] ]+', ' ', .) %>%
+                  gsub('[0-9]', '', .)%>%
+                  toupper() %>%
+                  gsub("CITY OF", "",.)%>%
+                  gsub("UA", "",.)%>%
+                  gsub("COUNTY OF", "",.)%>%
+                  gsub("ROYAL BOROUGH OF", "",.)%>%
+                  gsub("LEICESTER CITY", "LEICESTER",.)%>%
+                  gsub("UA", "",.)%>%
+                  gsub("DARWIN", "DARWEN", .)%>%
+                  gsub("AND DARWEN", "WITH DARWEN", .)%>%
+                  gsub("NE SOM", "NORTH EAST SOM", .)%>%
+                  gsub("N E SOM", "NORTH EAST SOM", .)%>%
+                  str_trim())%>%
+  dplyr::filter(category=="started during")%>%
+  dplyr::mutate(LA_Name = ifelse(LA_Name=="England", "England_current_year_reporting", LA_Name))
 
 
 
 
+    
+    
+rural <-     read.csv(curl("https://raw.githubusercontent.com/BenGoodair/childrens_social_care_data/00546ccef58bb7531f75b5b3b378b562af503b4b/Raw_Data/LA_level/Economic_Political_Contextual/17_10_2023_-_Rural-Urban_Classification_2011_lookup_tables_for_higher_level_geographies.csv"), skip=3)%>%
+              dplyr::mutate(LA_Name = Upper.Tier.Local.Authority.Area.2021.Name %>%
+                              gsub('&', 'and', .) %>%
+                              gsub('[[:punct:] ]+', ' ', .) %>%
+                              gsub('[0-9]', '', .)%>%
+                              toupper() %>%
+                              gsub("CITY OF", "",.)%>%
+                              gsub("UA", "",.)%>%
+                              gsub("COUNTY OF", "",.)%>%
+                              gsub("ROYAL BOROUGH OF", "",.)%>%
+                              gsub("LEICESTER CITY", "LEICESTER",.)%>%
+                              gsub("UA", "",.)%>%
+                              gsub("DARWIN", "DARWEN", .)%>%
+                              gsub("AND DARWEN", "WITH DARWEN", .)%>%
+                              gsub("NE SOM", "NORTH EAST SOM", .)%>%
+                              gsub("N E SOM", "NORTH EAST SOM", .)%>%
+                              gsub("COUNTY DURHAM", "DURHAM", .)%>%
+                              str_trim())
+  
 
 
 
